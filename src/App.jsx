890053efe52a8a8889666735802f8e5e4eb52dad
jamalmuradagaev <jamalmuradagaev@yandex.ru>
2024-03-12 23:50:08 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './Header/Header'
+
+export default function App() {
+
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
+
---
+++ src/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './style.css'
+
+export default function Header() {
+  return (
+    <header>
+      <div>
+        <p>About</p>
+        <p>Menu</p>
+        <p>Info</p>
+        <p>Booking</p>
+      </div>
+
+      <h1>Latte Cafe</h1>
+
+      <div>
+        
+      </div>
+    </header>
+  )
+}
